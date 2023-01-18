168e5e048ea4b07878af675f6fe0fb92bb2e40a6
Kiley Hector <kiley.hector@gmail.com>
2023-01-18 15:50:00 -0500

    yea, umm everything is broken.

---
+++ src/main/java/com/codeup/blogapp/RollDiceController.java
@@ -0,0 +1,38 @@
+package com.codeup.blogapp;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import java.util.Random;
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String showDiceRoll(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollDice(@PathVariable int n, Model model) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(6) + 1;
+        model.addAttribute("n", n);
+        model.addAttribute("randomNum", randomNum);
+        return "roll-dice";
+    }
+    @PostMapping("/roll-dice")
+    public void result(@RequestParam(name = "guess") int guess, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("/roll-dice/" + guess);
+    }
+
+
+    }
+
+
