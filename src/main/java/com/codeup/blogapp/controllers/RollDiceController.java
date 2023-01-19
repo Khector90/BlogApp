ab124a071b534e059129806ee6c294543ce23569
Kiley Hector <kiley.hector@gmail.com>
2023-01-19 10:34:57 -0500

    still working

--- src/main/java/com/codeup/blogapp/controllers/RollDiceController.java
+++ src/main/java/com/codeup/blogapp/controllers/RollDiceController.java
@@ -1,4 +1,4 @@
-package com.codeup.blogapp;
+package com.codeup.blogapp.controllers;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
