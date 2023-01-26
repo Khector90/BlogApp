a4704ddf3b4306d8186b0356b01a10d16082dec0
Kiley Hector <kiley.hector@gmail.com>
2023-01-26 17:40:54 -0500

    thank you kenneth

--- src/main/java/com/codeup/blogapp/models/User.java
+++ src/main/java/com/codeup/blogapp/models/User.java
@@ -30,9 +30,12 @@ public class User {
 
     }
 
-    public User(User user) {
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
     }
-
     public Long getId() {
         return id;
     }
